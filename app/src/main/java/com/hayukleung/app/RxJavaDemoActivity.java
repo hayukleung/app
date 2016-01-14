054b35e1d33c0ff8952291bc0f7310fdc7c6a042
hayukleung <hayukleung@gmail.com>
2016-01-14 17:26:33 +0800

    rxandroid: -- delete

--- app/src/main/java/com/hayukleung/app/RxJavaDemoActivity.java
+++ app/src/main/java/com/hayukleung/app/RxJavaDemoActivity.java
@@ -4,19 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
-import android.util.Log;
 import android.view.View;
 
 import com.hayukleung.app.util.ToastUtil;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.android.schedulers.HandlerScheduler;
-import rx.exceptions.OnErrorThrowable;
-import rx.functions.Func0;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
 
@@ -43,43 +42,7 @@ public class RxJavaDemoActivity extends Activity {
         findViewById(R.id.button_run_scheduler).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onRunSchedulerExampleButtonClicked();
                 test();
-            }
-        });
-    }
-
-    void onRunSchedulerExampleButtonClicked() {
-        sampleObservable()
-                // Run on a background thread
-                .subscribeOn(HandlerScheduler.from(backgroundHandler))
-                        // Be notified on the main thread
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<String>() {
-                    @Override public void onCompleted() {
-                        Log.d(TAG, "onCompleted()");
-                    }
-
-                    @Override public void onError(Throwable e) {
-                        Log.e(TAG, "onError()", e);
-                    }
-
-                    @Override public void onNext(String string) {
-                        Log.d(TAG, "onNext(" + string + ")");
-                    }
-                });
-    }
-
-    static Observable<String> sampleObservable() {
-        return Observable.defer(new Func0<Observable<String>>() {
-            @Override public Observable<String> call() {
-                try {
-                    // Do some long running operation
-                    Thread.sleep(TimeUnit.SECONDS.toMillis(5));
-                } catch (InterruptedException e) {
-                    throw OnErrorThrowable.from(e);
-                }
-                return Observable.just("one", "two", "three", "four", "five");
             }
         });
     }
@@ -98,22 +61,81 @@ public class RxJavaDemoActivity extends Activity {
                 subscriber.onCompleted();
             }
         });
-        Subscriber<String> subscriber = new Subscriber<String>() {
+//        Subscriber<String> subscriber = new Subscriber<String>() {
+//            @Override
+//            public void onCompleted() {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//                ToastUtil.showToast(RxJavaDemoActivity.this, s);
+//            }
+//        };
+//        observable.subscribe(subscriber);
+
+        // 假如并不关心OnComplete和OnError，我们只需要在onNext的时候做一些处理，这时候就可以使用Action1类
+//        Action1<String> actionNext = s -> ToastUtil.showToast(RxJavaDemoActivity.this, s);
+//        observable.subscribe(actionNext);
+
+        // Observable.just就是用来创建只发出一个事件就结束的Observable对象
+//        Observable.just("Hello RxJava")
+//                .subscribe(s -> ToastUtil.showToast(RxJavaDemoActivity.this, s));
+
+        // 操作符就是为了解决对Observable对象的变换的问题，操作符用于在Observable和最终的Subscriber之间修改Observable发出的事件
+//        Observable.just("hello")
+//                .map(s -> String.format("**%s**", s))
+//                .subscribe(s -> ToastUtil.showToast(RxJavaDemoActivity.this, s));
+
+        // Observable.from()方法，它接收一个集合作为输入，然后每次输出一个元素给subscriber
+        query("y")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<String>>() {
+                    @Override
+                    public void call(List<String> strings) {
+                        Observable
+                                .from(strings)
+                                .subscribe(new Action1<String>() {
+                                    @Override
+                                    public void call(String s) {
+                                        ToastUtil.showToast(RxJavaDemoActivity.this, s);
+                                    }
+                                });
+                    }
+                });
+    }
+
+    /**
+     * 搜索引擎
+     *
+     * @param keyword
+     *              输入y，查询得到result1，result2
+     *              输入其它，查询得到result3，result4
+     * @return
+     */
+    Observable<List<String>> query(String keyword) {
+        return Observable.create(new Observable.OnSubscribe<List<String>>() {
             @Override
-            public void onCompleted() {
-
+            public void call(Subscriber<? super List<String>> subscriber) {
+                if ("y".equals(keyword)) {
+                    List<String> results = new ArrayList<>(2);
+                    results.add("result1");
+                    results.add("result2");
+                    subscriber.onNext(results);
+                } else {
+                    List<String> results = new ArrayList<>(2);
+                    results.add("result3");
+                    results.add("result4");
+                    subscriber.onNext(results);
+                }
+                subscriber.onCompleted();
             }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                ToastUtil.showToast(RxJavaDemoActivity.this, s);
-            }
-        };
-        observable.subscribe(subscriber);
+        });
     }
 }
